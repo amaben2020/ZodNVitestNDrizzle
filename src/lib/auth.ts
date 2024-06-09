5c569360c5759faa211a4e554abe57787da44846
algomachine001 <amaben202phantom@gmail.com>
2024-06-09 11:23:54 +0100

    adding auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,25 @@
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import { createClient } from '@vercel/postgres';
+import { drizzle } from 'drizzle-orm/vercel-postgres';
 import GithubProvider from 'next-auth/providers/github';
-import config from './config';
-import { db } from './db';
+import * as schema from './schema';
+
+const client = createClient({
+  connectionString:
+    'postgres://default:CK3LWXf4iyoY@ep-tight-mode-66645246.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require',
+});
+
+const db = drizzle(client, { schema });
 
 export const authOptions = {
   adapter: DrizzleAdapter(db) as any,
-  secret: config.NEXTAUTH_SECRET,
+  debug: true,
+  secret: '1232j903vhv38vh384809',
   providers: [
     GithubProvider({
-      clientId: config.GITHUB_ID,
-      clientSecret: config.GITHUB_SECRET,
+      clientId: 'Ov23liA2hGQ4FW6RqMke',
+      clientSecret: '4d26c718bfe07312d1d6f1d9d55f22d49725af11',
+      // authorization: { params: { scope: 'read:user user:email' } },
     }),
   ],
 };
