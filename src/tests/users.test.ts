ee8ebd743418eae5c992d8922e574a268c94d6e9
algomachine001 <amaben202phantom@gmail.com>
2024-06-09 21:37:52 +0100

    adding unit testing 2

--- src/tests/users.test.ts
+++ src/tests/users.test.ts
@@ -1,9 +1,15 @@
 import { getUsers } from '@/helpers/getUsers';
 import { validateSchema } from '@/helpers/test/validateSchema';
 import { userZodSchema } from '@/schema/usersZodSchema';
-import { test } from 'vitest';
+import { expect, test } from 'vitest';
 
 test('It should return correct users', async () => {
   const data = await getUsers();
   validateSchema(data, userZodSchema as any);
+});
+
+test('id property should always be present', async () => {
+  const data = await getUsers();
+  const dataIsAvaillable = Array.isArray(data) && data.length > 0;
+  expect(dataIsAvaillable && data[0]).toHaveProperty('id');
 });
