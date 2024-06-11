100cfac3469956b680e90f3132de86fd110e15a4
algomachine001 <amaben202phantom@gmail.com>
2024-06-11 17:33:38 +0100

    adding user count logic

--- src/app/(dashboard)/people/page.tsx
+++ src/app/(dashboard)/people/page.tsx
@@ -1,8 +1,20 @@
 import { getUsers } from '@/helpers/getUsers';
 
 const page = async () => {
-  const data = await getUsers();
-  return <div>{JSON.stringify(data)}</div>;
+  const users = await getUsers();
+  return (
+    <div className="p-10 max-w-screen-xl">
+      <div className="flex justify-between items-center gap-4 flex-wrap">
+        {users?.data?.map(({ email, id, name, lastName }) => (
+          <div className="p-4 border min-h-10 w-[500px]" key={id}>
+            <h2 className="text-white">{lastName}</h2>
+            <p>{email}</p>
+          </div>
+        ))}
+      </div>
+      Total Users - {users?.totalItems}
+    </div>
+  );
 };
 
 export default page;
--- src/helpers/getUsers.ts
+++ src/helpers/getUsers.ts
@@ -1,10 +1,21 @@
 import { db } from '@/lib/db';
 import { users } from '@/lib/schema';
+import { count } from 'drizzle-orm';
 
-export const getUsers = async () => {
+export const getUsers = async (limit = 10, offset = 1) => {
   try {
-    const results = await db.select().from(users).limit(10).execute();
-    return results;
+    const results = await db.query.users
+      .findMany({
+        limit,
+        offset,
+      })
+      .execute();
+
+    const totalCount = await db.select({ values: count() }).from(users);
+    return {
+      data: results,
+      totalItems: totalCount[0].values,
+    };
   } catch (error) {
     if (error instanceof Error) {
       console.error(error);
