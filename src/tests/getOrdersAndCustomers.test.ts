11420d22f43812b59750f73c2b7414c56f79c331
algomachine001 <amaben202phantom@gmail.com>
2024-06-17 10:17:10 +0100

    adding test suites

--- src/tests/getOrdersAndCustomers.test.ts
+++ src/tests/getOrdersAndCustomers.test.ts
@@ -2,13 +2,19 @@ import {
   getOrdersAndCustomers,
   validateOrdersDataSchema,
 } from '@/helpers/getOrdersAndCustomers';
-import { validateSchema } from '@/helpers/test/validateSchema';
-import { describe, it } from 'vitest';
+import { validateList, validateSchema } from '@/helpers/test/validateSchema';
+import { describe, expect, it, test } from 'vitest';
 
 describe('It should properly validate the getOrdersAndCustomers helper', () => {
   it('It should return valid data', async () => {
     const data = await getOrdersAndCustomers();
 
     validateSchema(data, validateOrdersDataSchema as any);
+    validateList(data);
+  });
+
+  test.fails('fail test', async () => {
+    // more like opposite
+    await expect(false).rejects.toBe(true);
   });
 });
---
+++ src/tests/objects.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, it } from 'vitest';
+
+const userData = {
+  name: 'Ben',
+  age: 32,
+};
+describe('It should test 10 object methods', () => {
+  it('should return true if the object has property', () => {
+    expect(userData).toHaveProperty('age');
+  });
+
+  it('age should be greater than 30', () => {
+    expect(userData.age).toBeGreaterThan(30);
+  });
+
+  it('the result should equal the desired age', () => {
+    expect(userData.age).toEqual(32);
+  });
+  it.fails('should be able to expect a test to fail', () => {
+    expect(userData.age).not.toBe(32);
+  });
+});
